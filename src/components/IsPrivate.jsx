63f2a5644104a896d43007afd9c7a5d6527368a8
Peter R <prodger2@gmail.com>
2023-08-06 17:34:34 +0200

    Co-authored-by: Mariana-na <Mariana-na@users.noreply.github.com>

--- src/components/IsPrivate.jsx
+++ src/components/IsPrivate.jsx
@@ -1,20 +1,20 @@
-// import { useContext } from "react";
-// import { AuthContext } from "../context/Auth.context";
-// import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/Auth.context";
+import { useNavigate } from "react-router-dom";
 
-// function IsPrivate ({children}) {
-//     const {isLoading, isLoggedIn} = useContext(AuthContext);
-//     const navigate = useNavigate();
+function IsPrivate ({children}) {
+    const {isLoading, isLoggedIn} = useContext(AuthContext);
+    const navigate = useNavigate();
 
-//     if (isLoading) {
-//         return <p>Loading...</p>
-//     }
-//     if (!isLoggedIn) {
-//         navigate("/login");
-//     }
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
+    if (!isLoggedIn) {
+        navigate("/login");
+    }
 
-//     return <>{children}</>;
+    return <>{children}</>;
 
-// }
+}
 
-// export default IsPrivate;
+export default IsPrivate;
