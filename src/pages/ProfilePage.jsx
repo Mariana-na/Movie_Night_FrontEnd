f0d3b29f739cc35bb37e606985e93796c4d8047c
Hamed Jalili <itjalili82@gmail.com>
2023-08-07 15:21:51 +0200

    profilePage update

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -2,15 +2,63 @@ import React from 'react';
 import LogOut from '../components/LogOut';
 import {Link} from "react-router-dom";
 
+
+/////////////////////////// Hamed codes Start //////////////////////////
 function ProfilePage() {
+
+  const user = {
+    name: "",
+    email: "",
+  };
+
+  const events = [
+    { id: 1, title: "", date: "" },
+    { id: 2, title: "", date: "" },
+  ];
 
   return (
     <>
-      <div>ProfilePage</div>
-      <Link to="/eventCreation">Create Event</Link>
-      <LogOut/>
+      <div>
+        <h2>Profile Page</h2>
+        <p>
+          Username: {user.name}
+          <br />
+          Email: {user.email}
+        </p>
+      </div>
+
+      <div>
+        <h3>My Events</h3>
+        <ul>
+          {events.map((event) => (
+            <li key={event.id}>
+              {event.title} - {event.date}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <Link to="/eventCreationPage">Create Event</Link>
+
+      <LogOut />
     </>
-  )
+  );
 }
+
+/////////////////////////// Hamed codes End //////////////////////////
+
+
+// function ProfilePage() {
+
+//   return (
+//     <>
+//       <div>ProfilePage</div>
+//       <Link to="/eventCreation">Create Event</Link>
+  
+//         {/* Log out button */}
+//         <LogOut />
+//     </>
+//     );
+// }
 
 export default ProfilePage
