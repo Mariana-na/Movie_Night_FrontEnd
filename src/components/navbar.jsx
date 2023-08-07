9fba9c36f7e6c1cb7f9b1b9998498076e0000aab
Mariana Ribeiro de Carvalho <mariana.rcarvalho.992gmail.com>
2023-08-07 11:43:18 +0100

    clean

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -13,19 +13,6 @@ function navbar() {
             <Link to= "/"><button>Home</button></Link> {/*Make it not show up in HomePage */}
 
 
-                {/*
-                {!isLoading ? (
-                    isLoggedIn ? (
-                    <><Link to="/profile/:userId"><button>Profile</button></Link>
-                    <button onClick={logoutUser}>Logout</button>
-                    <span>{user && user.name}</span></>
-                   ) : (
-                    <><Link to="/signup"> <button>Sign Up</button> </Link>
-                    <Link to="/login"> <button>Login</button> </Link></>)
-                )}
-                */}
-
-
                 {!isLoading ? (
                     isLoggedIn ? (
                       <>
@@ -46,27 +33,6 @@ function navbar() {
                       </>
                     )
                   ) : null}
-                  
-
-
-                {/*
-                {isLoggedIn && (
-                    <>
-                    <Link to="/profile/:userId"><button>Profile</button></Link> 
-                    <button onClick={logoutUser}>Logout</button>
-                    <span>{user && user.name}</span>
-                    </>
-                )}
-
- 
-                {!isLoggedIn && (
-                    <>
-                    <Link to="/signup"> <button>Sign Up</button> </Link>
-                    <Link to="/login"> <button>Login</button> </Link>
-                    </>
-                )}
-                */}
-
 
         </nav>
 
