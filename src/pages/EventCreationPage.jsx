4b3af6162a0f89bd4c03654aa0d37307239696b8
Peter R <prodger2@gmail.com>
2023-08-08 18:05:55 +0200

    Can add an event to the database

--- src/pages/EventCreationPage.jsx
+++ src/pages/EventCreationPage.jsx
@@ -3,52 +3,79 @@ import EventLogistics from "../components/EventLogistics";
 import MovieInfo from "../components/MovieInfo";
 import {useState} from "react";
 import NavBar from "../components/NavBar";
-//import {API_URL} from "../config/config.index";
+import axios from "axios";
+import { API_URL } from "../config/config.index";
+import { useNavigate } from "react-router-dom";
+//import { AuthContext } from "../context/Auth.context";
+//import { useContext } from "react";
+
+
 
 function EventCreationPage() {
 
  // const [eventLogistics, setEventLogistics] = useState(null);
   
 // Information from the EventLogistics component
+  const [eventName, setEventName] = useState("");
   const [when, setWhen] = useState("");
   const [where, setWhere] = useState("");
   const [who, setWho] = useState("");
 
 // Information from the RecipeInfo component
   const [randomMeal, setRandomMeal] = useState(null);
+const navigate = useNavigate();
 
+ // const { user } = useContext(AuthContext);
+  
+  const handleEventCreation = async (event) => {
+        event.preventDefault()
+
+        try {
+            const newEvent = await axios.post(`${API_URL}/event/createEvent`, {eventName, when, where, who, randomMeal})
+
+            console.log("event creation response", newEvent)
+
+            navigate("/eventDetails");
+
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
 
   const handleFormSubmit = (event) => {
     // Handle the form data here
     event.preventDefault();
-  
+    handleEventCreation(event);
   };
-  return (
-    <div>
-      <NavBar/>
-      <MovieInfo/>
-      <RecipeInfo randomMeal={randomMeal} setRandomMeal={setRandomMeal} />
-      <form onSubmit={handleFormSubmit}>
-        {/* When the user clicks on this – the entire event is created using all the info */}
-        <EventLogistics when={when} setWhen={setWhen} where={where} setWhere={setWhere} who={who} setWho={setWho} />
-        <button type="submit">Confirm</button>
-      </form>
-    </div>
-  );
-}
 
-{/*
   return (
     <>
+      <div>
+        <NavBar />
 
-      <h1>Movie Night</h1>
-
-      <MovieInfo/>
-      <RecipeInfo />
-      <EventLogistics />
+        <form onSubmit={handleFormSubmit}>
+          {/* When the user clicks on this – the entire event is created using all the info */}
+          <MovieInfo />
+          <RecipeInfo randomMeal={randomMeal} setRandomMeal={setRandomMeal} />
+          <EventLogistics
+            eventName={eventName}
+            setEventName={setEventName}
+            when={when}
+            setWhen={setWhen}
+            where={where}
+            setWhere={setWhere}
+            who={who}
+            setWho={setWho}
+          />
+          <button type="submit">Confirm</button>
+        </form>
+      </div>
+      <p>When {when}</p>
+      <p>Where {where}</p>
+      <p>Who {who}</p>
     </>
   );
 }
-*/}
 
 export default EventCreationPage;
