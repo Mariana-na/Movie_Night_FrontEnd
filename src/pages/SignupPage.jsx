7f82f1cff10172bdaedf2d541702dfb7f4fcad1b
Hamed Jalili <itjalili82@gmail.com>
2023-08-11 10:00:01 +0200

    update

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,31 +1,22 @@
 import { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import "../assets/style/SignupPage.css"
-
+import {API_URL} from "../config/config.index";
 function SignupPage() {
-
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     // eslint-disable-next-line no-unused-vars
     const [errorMessage, setErrorMessage] = useState(undefined);
-
     const navigate = useNavigate();
-
     const handleSignup = async (event) => {
         event.preventDefault()
-
         try {
             const response = await axios.post(`${API_URL}/auth/signup`, {name, email, password})
-
             console.log("signup response", response)
-
             navigate("/login");
-
         } catch (error) {
             console.log(error);
-            
         }
     }
 
@@ -37,18 +28,15 @@ function SignupPage() {
 <Link to={"/"} className="home-btn">Home</Link>
             <h1 className="signup-title"><b>Movie</b>Night</h1>
             <form onSubmit={handleSignup} className="signup-form" >
-                <label>
-                    First Name:
-                    <input placeholder="Your real name. Or a nickname" type="text" required value={firstname} onChange={(event) => {setFirstname(event.target.value); }}/>
+
+            <label>
+            <p className="signup-text">Name:</p>
+                    <input placeholder="Your real name. Or a nickname" type="text" required value={name} onChange={(event) => {setName(event.target.value); }}/>
                 </label>
                 <br />
+
                 <label>
-                    Last Name:
-                    <input placeholder="Makes it easier for your friends to recognise you" type="text" value={lastname} onChange={(event) => {setLastname(event.target.value); }}/>
-                </label>
-                <br />
-                <label>
-                    Email:
+                    <p className="signup-text"> Email:</p>
                     <input type="text" required value={email} onChange={(event) => {setEmail(event.target.value); }}/>
                 </label>
                 <br />
